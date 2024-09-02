adb1380b49051fd6a147658080627a10b506f1fd
RonaldoMaia <ronaldomaiacorrea@outlook.com>
2024-09-02 12:24:29 -0700

    Add base url to fit the root project name into Vte config

--- technical-assessment/vite.config.ts
+++ technical-assessment/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import { UserConfigExport } from 'vite';
 
 export default defineConfig({
+	base: '/scrawlr-assessment/technical-assessment/',
 	test: {
 		globals: true,
 		environment: 'jsdom',
